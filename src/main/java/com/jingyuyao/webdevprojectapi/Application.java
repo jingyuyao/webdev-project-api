6f929bdf721c07f0f35012374e69258d07f2e018
Jingyu Yao <yjy.emails@gmail.com>
2018-06-05 22:54:41 -0400

    renamed starter package and class

--- src/main/java/com/jingyuyao/webdevprojectapi/Application.java
+++ src/main/java/com/jingyuyao/webdevprojectapi/Application.java
@@ -1,12 +1,12 @@
-package com.example.webdevprojectapi;
+package com.jingyuyao.webdevprojectapi;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class DemoApplication {
+public class Application {
 
 	public static void main(String[] args) {
-		SpringApplication.run(DemoApplication.class, args);
+		SpringApplication.run(Application.class, args);
 	}
 }
