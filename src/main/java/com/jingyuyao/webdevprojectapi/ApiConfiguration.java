f078d67a1a7fe5bc091da6469ff012451f06b336
Jingyu Yao <yjy.emails@gmail.com>
2018-06-05 23:58:24 -0400

    added cors settings

---
+++ src/main/java/com/jingyuyao/webdevprojectapi/ApiConfiguration.java
@@ -0,0 +1,25 @@
+package com.jingyuyao.webdevprojectapi;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+@Configuration
+public class ApiConfiguration {
+
+  @Bean
+  public WebMvcConfigurer webMvcConfigurer() {
+    return new WebMvcConfigurer() {
+      @Override
+      public void addCorsMappings(CorsRegistry registry) {
+        registry
+            .addMapping("/**")
+            .allowedMethods("*")
+            .allowedOrigins(
+                "http://localhost:4200",
+                "https://jingyuyao-webdev-project.herokuapp.com");
+      }
+    };
+  }
+}
---
+++ src/main/java/com/jingyuyao/webdevprojectapi/service/PingService.java
@@ -0,0 +1,22 @@
+package com.jingyuyao.webdevprojectapi.service;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class PingService {
+
+  @GetMapping("/ping")
+  public Pong ping() {
+    return new Pong();
+  }
+
+  private class Pong {
+
+    private final String data = "pong";
+
+    public String getData() {
+      return data;
+    }
+  }
+}
